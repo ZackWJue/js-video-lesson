d49a05412d549a7d68ec033a970a0b6811c20453
ZackWJue <zackwjue@gmail.com>
2023-12-14 19:43:37 -0600

    lll

---
+++ fundamentals.js
@@ -0,0 +1,54 @@
+/*
+
+conditionals (if/else)
+for loops
+while loops
+arrays/array methods
+objects/classes
+functions
+
+
+*/
+ 
+
+/* brandon jumping in and starting me on conditionals*/
+
+const answer = "penis";
+
+if(answer === "works"){
+    console.log("it worked")
+    
+}
+else if(answer === "option1"){
+    console.log("")
+}
+else if(answer === "option2"){
+    console.log("")
+}
+else if(answer === "option3"){
+    console.log("")
+}
+else{
+    console.log("nothing else worked")
+}
+
+
+if() {
+    
+}
+else if (){
+
+}
+else if() {
+
+}
+else if(){
+
+}
+else{
+    
+}
+
+//if answer equals "story"
+//      console.log (option 1 option 2)
+//if 
