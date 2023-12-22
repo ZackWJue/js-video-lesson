e8de1ecf878b4e6a5d0334e82461e09979896896
ZackWJue <153241386+ZackWJue@users.noreply.github.com>
2023-12-21 18:21:01 -0600

    Update fundamentals.js
    new assignment from leo
    
    contacts list

--- fundamentals.js
+++ fundamentals.js
@@ -25,6 +25,8 @@ git push
 
 */
 
+/*
+
 const answer = "penis";
 
 if(answer === "works"){
@@ -42,9 +44,10 @@ else if(answer === "option3"){
 }
 else{
     console.log("nothing else worked")
-}
+} 
 
 
+// Use this for a template to copy and paste//
 if() {
     
 }
@@ -59,8 +62,28 @@ else if(){
 }
 else{
     
+} 
+
+*/
+
+/* !!NEW ASSIGNMENT UPDATE!!
+
+LEO: Please write me a program that starts witjh a variable called "phoneNumber"
+
+If phoneNumber is equal to 9179407943, return "thats's Leo's number."
+
+If phoneNumber is equal to 6097513904, return " that's Brandon's number."
+
+For any other value, return "not in your contacts"
+
+ END TRANSMISSION  */
+if(answer === "9179407943"){
+    console.log("that's Leo's number.")
+}
+else if(answer === "6097513904"){
+    console.log("that's Brandon's number.")
+}
+else(){
+    console.log("not in your contacts")
 }
 
-//if answer equals "story"
-//      console.log (option 1 option 2)
-//if 
